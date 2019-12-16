3aa19f84c6fff0301b29b219b944f4905786b251
Paul DAO <dao.van.paul@gmail.com>
2019-12-16 10:19:53 +0100

    Create a global router

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 const express = require('express');
-const comments = require('./routes/comments');
-const posts = require('./routes/posts');
+// const comments = require('./routes/comments'); // Déplacer dans routes/index
+// const posts = require('./routes/posts'); // Déplacer dans routes/index
+const api = require('./routes'); // Dans notre cas, pas besoin de préciser .routes/index
+// car NodeJs va automatiquement chercher un index.js
+
 
 const app = express();
 const port = 8000;
 
-// // Get a list of posts
-// app.get('/api/posts', (req, res) => {
-//   res.json(fakePosts);
-// });
 
-// // Get a single post
-// app.get('/api/posts/:id', (req, res) => {
-//   // Find the post in the array that has the id given by req.params.id
-//   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-//   const postId = Number(req.params.id);
-//   const foundPost = fakePosts.find((post) => post.id === postId);
-//   if (!foundPost) {
-//     return res.status(404).json({
-//       error: 'Post not found',
-//     });
-//   }
-//   return res.json(foundPost);
-// });
+app.use('/api', api); // permet de connecter le router comments et ses routes au système de routage fourni par app
 
-app.use('/api/comments', comments); // permet de connecter le router et ses routes au système de routage fourni par app
-app.use('/api/posts', posts);
 app.listen(port, (err) => {
   if (err) {
     throw new Error('Something bad happened...');
   }
+  // eslint-disable-next-line no-console
   console.log(`Server is listening on ${port}`);
 });
